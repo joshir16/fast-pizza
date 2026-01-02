efcaebe2a3af6f8ca366ce51dc67712fb2df8079
Rishabh <joshir838@gmail.com>
2026-01-02 18:50:08 +0530

    redux- user reducer

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
   return (
     <p className="hidden text-sm font-semibold md:block">
       <span className="mr-1 text-stone-800 font-medium">hello,</span>
-      Rishabh
+      {username}
     </p>
   );
 }
